f79ff180e0b1a47bab7995bb84829efce2175d3b
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-22 13:23:07 -0400

    Testing AJAX API call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,38 +3,58 @@ const apiTokenEndpoint = 'https://accounts.spotify.com/api/token'
 const clientId = '4fd7a3464d9a42b3b839a4db644067c4'
 const clientSecret = 'de44091eeac14f2290afee5f5156b863'
 
-$.ajax({
-  type: 'POST',
-  url: apiTokenEndpoint,
-  crossDomain: true,
-  data: {
-    'grant_type': 'client_credentials'
+const returnBasic = function (id, secret) {
+  return 'Basic ' + btoa(id + ':' + secret)
+}
+
+var settings = {
+  'async': true,
+  'crossDomain': true,
+  'url': 'https://accounts.spotify.com/api/token',
+  'method': 'POST',
+  'headers': {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': returnBasic(clientId, clientSecret),
   },
-  beforeSend: function (xhr) {
-    xhr.setRequestHeader('Authorization', btoa(clientId + ':' + clientSecret))
+  'body': {
+    'grant_type': 'client_credentials'
   }
-}).then((response) => {
+}
+
+$.ajax(settings).then(function (response) {
   console.log(response)
 })
 
-const searchArtist = function searchArtist(artistName) {
+// $.ajax({
+//   type: 'POST',
+//   url: apiTokenEndpoint,
+//   crossDomain: true,
+//   body: {
+//     'grant_type': 'client_credentials'
+//   },
+//   headers: {'Authorization': 'Basic' + window.btoa(clientId + ':' + clientSecret)}
+// }).then((response) => {
+//   console.log(response)
+// })
+
+const searchArtist = function searchArtist (artistName) {
   const fullUrl = url + $.param({
     q_artist: artistName
   })
 
-  $.ajax({
-    url: fullUrl,
-    method: 'GET',
-    beforeSend: (xhr) => {
-      /* Authorization header */
-      xhr.setRequestHeader('Authorization', apiKey)
-    }
-  }).then((response) => {
-    console.log(response)
-  })
+  // $.ajax({
+  //   url: fullUrl,
+  //   method: 'GET',
+  //   beforeSend: (xhr) => {
+  //     /* Authorization header */
+  //     xhr.setRequestHeader('Authorization', apiKey)
+  //   }
+  // }).then((response) => {
+  //   console.log(response)
+  // })
 }
 
-const createResults = function createResults(artistObj) {
+const createResults = function createResults (artistObj) {
   return true
 }
 
