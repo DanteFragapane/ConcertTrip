3249465251e1015eaf7752cd9e4330d29019105f
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-22 20:54:17 -0400

    Got a search for artist call working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,25 @@
 const url = 'https://api.spotify.com/v1/search?'
-const apiTokenEndpoint = 'https://accounts.spotify.com/api/token'
+const apiTokenEndpoint = 'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token'
 const clientId = '4fd7a3464d9a42b3b839a4db644067c4'
 const clientSecret = 'de44091eeac14f2290afee5f5156b863'
+let accessToken = ''
 
 const returnBasic = function (id, secret) {
-  return 'Basic ' + btoa(id + ':' + secret)
+  return 'Basic ' + window.btoa(id + ':' + secret)
 }
 
 var settings = {
   'async': true,
   'crossDomain': true,
-  'url': 'https://accounts.spotify.com/api/token',
-  dataType: 'jsonp',
+  'url': 'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token?grant_type=client_credentials',
   'method': 'POST',
   'headers': {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': returnBasic(clientId, clientSecret),
-  },
-  'body': {
-    'grant_type': 'client_credentials'
+    'Authorization': returnBasic(clientId, clientSecret)
   }
 }
 
-$.ajax(settings).then(function (response) {
-  console.log(response)
-})
+
 
 // $.ajax({
 //   type: 'POST',
@@ -38,9 +33,26 @@ $.ajax(settings).then(function (response) {
 //   console.log(response)
 // })
 
-const searchArtist = function searchArtist (artistName) {
+const searchArtist = function searchArtist(artistName) {
   const fullUrl = url + $.param({
-    q_artist: artistName
+    q: artistName,
+    type: 'artist'
+  })
+  console.log(fullUrl)
+  $.ajax(settings).then(function (response) {
+    accessToken = response.access_token
+    console.log(accessToken)
+
+    $.ajax({
+      url: fullUrl,
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }).then((response) => {
+      const artist = response.artists.items[0]
+      console.log(artist)
+    })
   })
 
   // $.ajax({
@@ -55,7 +67,7 @@ const searchArtist = function searchArtist (artistName) {
   // })
 }
 
-const createResults = function createResults (artistObj) {
+const createResults = function createResults(artistObj) {
   return true
 }
 
