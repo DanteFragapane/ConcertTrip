171dd53c0fce0896fb2c92e0be902614207ade2b
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-22 21:25:58 -0400

    Added a few more API URL's, updated the name for Spotify's URL

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
+
+const spotify = 'https://api.spotify.com/v1/search?'
+const ticketMaster = 'https://app.ticketmaster.com/discovery/v2/'
+const songKick = 'https://api.songkick.com/api/3.0/'
+
 const clientId = '4fd7a3464d9a42b3b839a4db644067c4'
 const clientSecret = 'de44091eeac14f2290afee5f5156b863'
 let accessToken = ''
@@ -18,17 +23,16 @@ var settings = {
 }
 
 const searchArtist = function searchArtist(artistName) {
-  const fullUrl = url + $.param({
+  const spotifyFull = spotify + $.param({
     q: artistName,
     type: 'artist'
   })
-  console.log(fullUrl)
+  console.log(spotifyFull)
   $.ajax(settings).then(function (response) {
     accessToken = response.access_token
     console.log(accessToken)
-
     $.ajax({
-      url: fullUrl,
+      url: spotifyFull,
       method: 'GET',
       headers: {
         'Authorization': 'Bearer ' + accessToken
