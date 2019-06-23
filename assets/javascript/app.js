276b26c8acaf183cb718c8b73f1d060d84bacdb8
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-22 22:11:55 -0400

    Working on adding in SongKick's API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,15 @@
 
 const spotify = 'https://api.spotify.com/v1/search?'
 const ticketMaster = 'https://app.ticketmaster.com/discovery/v2/'
-const songKick = 'https://api.songkick.com/api/3.0/'
+const songKick = 'https://api.songkick.com/api/3.0'
 
 const clientId = '4fd7a3464d9a42b3b839a4db644067c4'
 const clientSecret = 'de44091eeac14f2290afee5f5156b863'
 let accessToken = ''
+const apiSongKick = ''
 
 const returnBasic = function (id, secret) {
   return 'Basic ' + window.btoa(id + ':' + secret)
-}
-
-var settings = {
-  'async': true,
-  'crossDomain': true,
-  'url': 'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token?grant_type=client_credentials',
-  'method': 'POST',
-  'headers': {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': returnBasic(clientId, clientSecret)
-  }
 }
 
 const searchArtist = function searchArtist(artistName) {
@@ -27,8 +17,21 @@ const searchArtist = function searchArtist(artistName) {
     q: artistName,
     type: 'artist'
   })
-  console.log(spotifyFull)
-  $.ajax(settings).then(function (response) {
+  const songKickFull = songKick + '/search/artist.json?' + $.param({
+    apikey: apiSongKick,
+    query: artistName
+  })
+
+  $.ajax({
+    'async': true,
+    'crossDomain': true,
+    'url': 'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token?grant_type=client_credentials',
+    'method': 'POST',
+    'headers': {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': returnBasic(clientId, clientSecret)
+    }
+  }).then(function (response) {
     accessToken = response.access_token
     console.log(accessToken)
     $.ajax({
@@ -41,6 +44,14 @@ const searchArtist = function searchArtist(artistName) {
       const artist = response.artists.items[0]
       console.log(artist)
     })
+  })
+
+
+  $.ajax({
+    url: songKickFull,
+    method: 'GET'
+  }).then((response) => {
+    console.log(response)
   })
 }
 
